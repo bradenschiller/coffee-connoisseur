b68c1d05965545a5ff17e48665378d7f51f95f16
braden <26287414+bradenschiller@users.noreply.github.com>
2021-12-05 12:07:43 -0600

    add hello test page & remove unwanted README file

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <div>Add to every page</div>
+    </>
+  );
 }
 
 export default MyApp
---
+++ pages/hello.js
@@ -0,0 +1,7 @@
+import styles from "../styles/hello.module.css";
+
+const hello = () => {
+  return <div className={styles.hello}>Hello World</div>;
+};
+
+export default hello;
